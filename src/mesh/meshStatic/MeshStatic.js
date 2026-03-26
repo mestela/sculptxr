7ff2e411f482358a01047495059bf1a95894a91b
Matt Estela <matt@tokeru.com>
2026-03-27 08:08:52 +1100

    [PARANOID] v1.0.52 - Remove legacy Voxel Shader Overrides in MeshStatic

--- src/mesh/meshStatic/MeshStatic.js
+++ src/mesh/meshStatic/MeshStatic.js
@@ -74,20 +74,6 @@ class MeshStatic extends Mesh {
     super.updateWireframeBuffer();
   }
 
-
-  setShaderType(type) {
-    if (this._isVoxel && type !== Enums.Shader.FLAT && type !== Enums.Shader.WIREFRAME) type = Enums.Shader.FLAT;
-    super.setShaderType(type);
-  }
-
-  getShaderType() {
-    if (this._isVoxel) {
-      // If underlying is WIREFRAME, return it.
-      if (this._renderData && this._renderData._shaderType === Enums.Shader.WIREFRAME) return Enums.Shader.WIREFRAME;
-      return Enums.Shader.FLAT;
-    }
-    return super.getShaderType();
-  }
 }
 
 export default MeshStatic;
